2321b838aef49126dcfae4a81ffbbc73648dcdd1
ridzuan azmi <ridzuanazmi.11@gmail.com>
2024-04-18 12:21:39 +0800

    complete

--- src/App.java
+++ src/App.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,20 +11,46 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		Scanner scanner = new Scanner(System.in); // Create a Scanner object
 		String action = "";
-		printMenu();
+		String user = null;
 		ArrayList<String> shoppingCart = new ArrayList<String>(); // Create an ArrayList object
-		
+		// File cartDBFolder = retrieveCartDBFolder(); // Create a folder to store the shopping cart database if it does not exist											
+		printMenu();
+
 		while (action != "exit") {
 			System.out.print("> ");
 			action = scanner.next().trim().toLowerCase(); // Read user input
 			switch (action) {
-				case "list":
-					if (shoppingCart.isEmpty()) {
-						System.out.println("Your shopping cart is empty.");
+				case "login":
+					String username = scanner.nextLine().trim(); // Read the username. If the username is empty, prompt the user to enter a username
+					if (username.length() != 0) {
+						user = username;
 					} else {
-						System.out.println("Here are the items in your shopping cart: ");
-						listItem(shoppingCart);
+						System.out.println("Please enter a username.");
+						break;
 					}
+					System.out.printf("Welcome %s\n", user);
+					getCartDBFile(user); // Retrieve the shopping cart from the database
+					break;
+				case "save":
+					if (!ensureUserLoggedIn(user) || !ensureCartNotEmpty(shoppingCart)) {
+						break;
+					}
+					System.out.println("User: " + user + "\nShopping Cart: " + shoppingCart);
+					saveShoppingCartToFile(user, shoppingCart);
+					shoppingCart.clear();
+					break;
+				case "users":
+					if (!ensureUsersInCartDB()) {
+						break;
+					}
+					checkUsersInDb();
+					break;
+				case "list":
+					if (!ensureUserLoggedIn(user)) {
+						break;
+					}
+					checkCurrentShoppingCart(shoppingCart);
+					checkUserCartContents(user);
 					break;
 				case "add":
 					String itemsToAdd = scanner.nextLine().trim();
@@ -49,8 +81,132 @@ public class App {
 					break;
 			}
 		}
-
 		scanner.close();
+	}
+
+	private static void getCartDBFile(String user) {
+		ArrayList<String> shoppingCart = new ArrayList<String>();
+		File cartDBFile = new File("cartDB\\" + user + ".txt");
+		if (cartDBFile.exists()) {
+			try (BufferedReader bufferedReader = new BufferedReader(new FileReader(cartDBFile))) {
+				String line;
+				if (0 == cartDBFile.length()) {
+					System.out.println("You are already registered. " + user + ", your shopping cart is empty.");
+				}
+				while ((line = bufferedReader.readLine()) != null) {
+					shoppingCart.add(line);
+				}
+				if (!shoppingCart.isEmpty())
+					System.out.println("Contents of your shopping cart: " + shoppingCart);					
+				bufferedReader.close();
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				cartDBFile.createNewFile();
+				System.out.println(user + ", your shopping cart is empty.");
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static boolean ensureUsersInCartDB() {
+		File cartDBFolder = new File("cartDB");
+		if (cartDBFolder.list().length == 0) {
+			System.out.println("No users are registered.");
+			return false;
+		}
+		return true;
+	}
+
+	private static void checkCurrentShoppingCart(ArrayList<String> shoppingCart) {
+		if (shoppingCart.isEmpty()) {
+			System.out.println("Your shopping cart is empty.");
+		} else {
+			System.out.printf("Your shopping cart contains: %s\n", shoppingCart);
+		}
+	}
+
+	private static void checkUserCartContents(String user) {
+		File cartDBFile = new File("cartDB\\" + user + ".txt");
+		ArrayList<String> shoppingCart = new ArrayList<String>();
+		if (cartDBFile.exists()) {
+			try (BufferedReader bufferedReader = new BufferedReader(new FileReader(cartDBFile))) {
+				String line;
+				if (0 == cartDBFile.length()) {
+					System.out.println(user + ", your shopping cart in the DB is empty.");
+				}
+				while ((line = bufferedReader.readLine()) != null) {
+					shoppingCart.add(line);
+				}
+				if (!shoppingCart.isEmpty())
+					System.out.println("Current contents of your shopping cart: " + shoppingCart);					
+				bufferedReader.close();
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("User: " + user + " does not exist.");
+		}
+	}
+
+	private static void checkUsersInDb() {
+		File cartDBFolder = new File("cartDB");
+		String[] files = cartDBFolder.list();
+		for (int i = 0; i < files.length; i++) {
+			System.out.printf("%d. %s\n", i + 1, files[i].substring(0, files[i].indexOf(".txt")));
+		}
+	}
+
+	private static boolean ensureUserLoggedIn(String user) {
+		if (user == null) {
+			System.out.println("Please login to save or see your shopping cart.");
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean ensureCartNotEmpty(ArrayList<String> shoppingCart) {
+		if (shoppingCart.isEmpty()) {
+			System.out.println("Your shopping cart is empty. Nothing to save.");
+			return false;
+		}
+		return true;
+	}
+
+	private static void saveShoppingCartToFile(String user, ArrayList<String> shoppingCart) {
+		File cartDBFile = new File("cartDB\\" + user + ".txt");
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(cartDBFile, true))) {
+			for (String item : shoppingCart) {
+				bufferedWriter.write(item);
+				bufferedWriter.newLine();
+			}
+			bufferedWriter.flush();
+			bufferedWriter.close();
+			System.out.println(user + ", your shopping cart has been saved successfully.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
+
+	private static File retrieveCartDBFolder() {
+		File cartDBFolder = new File("cartDB");
+		if (!cartDBFolder.exists()) {
+			if (cartDBFolder.mkdir()) {
+				System.out.println("cartDB folder created successfully.");
+			} else {
+				System.out.println("Failed to create cartDB folder.");
+			}
+		} else {
+			System.out.println("cartDB folder already exists.");
+		}
+		return cartDBFolder;
 	}
 
 	private static void removeItemFromCart(ArrayList<String> shoppingCart, String itemToDelete) {
@@ -83,6 +239,9 @@ public class App {
 		System.out.print("""
 				---------------Welcome to your shopping cartItems---------------
 				Type in what you want to do
+				login - Login to your account or create an account
+				save - Save your shopping cart
+				users - List all users in the database
 				list - List all items in your shopping cart
 				add - Add item(s) to your shopping cart
 				remove - Remove item(s) from your shopping cart
